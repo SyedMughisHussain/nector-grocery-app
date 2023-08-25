5c0339ce53f585356803f87f6c5b219222720997
SyedMuhgisHussain <syedmughis4541@gmail.com>
2023-08-25 14:16:48 +0500

    changes made

--- lib/pages/others/detail_page.dart
+++ lib/pages/others/detail_page.dart
@@ -1,16 +1,162 @@
 import 'package:flutter/material.dart';
+import 'package:nector_app/utils/colors.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 // ignore: must_be_immutable
 class DetailPage extends StatelessWidget {
-  var _product;
-  DetailPage(this._product, {super.key});
+  // ignore: prefer_typing_uninitialized_variables
+  var product;
+  DetailPage(this.product, {super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(_product['productName']),
-      ),
-    );
+        body: Column(
+      children: [
+        Column(children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 30),
+            child: Container(
+              color: Colors.grey[300],
+              child: Row(
+                children: [
+                  IconButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      icon: const Icon(
+                        Icons.arrow_back_ios_new,
+                        color: AppColors.blackColor,
+                      ))
+                ],
+              ),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            height: 250,
+            decoration: BoxDecoration(
+                borderRadius: const BorderRadius.only(
+                    bottomLeft: Radius.circular(30),
+                    bottomRight: Radius.circular(30)),
+                color: Colors.grey[300]),
+            child: Image.network(
+              product['imageUrl'],
+              fit: BoxFit.cover,
+              height: 220,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 15, left: 15, right: 15),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      product['productName'],
+                      style: const TextStyle(
+                          color: AppColors.blackColor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25),
+                    ),
+                    IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.favorite_border))
+                  ],
+                ),
+                Text(
+                  product['productQuantity'],
+                  style: const TextStyle(color: AppColors.darkGreyColor),
+                ),
+                Container(
+                  alignment: Alignment.topRight,
+                  child: Text(
+                    '\$${product['productPrice']}',
+                    style: const TextStyle(
+                        color: AppColors.blackColor,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                const Divider(
+                  color: AppColors.darkGreyColor,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text(
+                  'Product Detail',
+                  style: TextStyle(
+                      color: AppColors.blackColor,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 18),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  product['productDetail'],
+                  style: const TextStyle(color: AppColors.darkGreyColor),
+                ),
+                const Divider(
+                  color: AppColors.darkGreyColor,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      'Nutritions',
+                      style: TextStyle(
+                          color: AppColors.blackColor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18),
+                    ),
+                    Text(
+                      product['nutritions'],
+                      style: TextStyle(
+                          color: AppColors.blackColor,
+                          backgroundColor: Colors.grey[300]),
+                    )
+                  ],
+                ),
+                const Divider(
+                  color: AppColors.darkGreyColor,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      'Review',
+                      style: TextStyle(
+                          color: AppColors.blackColor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18),
+                    ),
+                    RatingBar.builder(
+                      initialRating: 3,
+                      minRating: 1,
+                      direction: Axis.horizontal,
+                      allowHalfRating: true,
+                      itemCount: 5,
+                      itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
+                      itemBuilder: (context, _) => const Icon(
+                        Icons.star,
+                        size: 15,
+                        color: Colors.amber,
+                      ),
+                      onRatingUpdate: (rating) {
+                        print(rating);
+                      },
+                    )
+                  ],
+                ),
+              ],
+            ),
+          )
+        ])
+      ],
+    ));
   }
 }
